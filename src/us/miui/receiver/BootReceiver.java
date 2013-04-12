8934aad52c0cb4f8d315cc0a8ca80a96ebff5905
0xD34D <clark@scheffsblend.com>
2013-04-12 10:42:41 -0700

    Updating for public release

--- src/us/miui/receiver/BootReceiver.java
+++ src/us/miui/receiver/BootReceiver.java
@@ -16,6 +16,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.util.Log;
+import us.miui.toolbox.RootUtils;
 
 /**
  * @author Clark Scheff
@@ -94,6 +95,5 @@ public class BootReceiver extends BroadcastReceiver {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
+    }
 }
